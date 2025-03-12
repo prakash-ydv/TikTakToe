9d90abf39a597ecba31c752568f237f201e06714
Prakash Kumar <prakashyadav9576@gmail.com>
2025-03-12 10:41:54 +0530

    reduce size of cells

--- components/Square.jsx
+++ components/Square.jsx
@@ -4,7 +4,7 @@ function Square(props) {
   return (
     <div
       onClick={props.clickHandler}
-      className={`${props.isWinningCell ? (props.winner == "X" ? "bg-green-500" : "bg-red-500") : "bg-zinc-950"} h-28 w-28 border-2 border-white rounded-2xl flex items-center justify-center cursor-pointer select-none`}
+      className={`${props.isWinningCell ? (props.winner == "X" ? "bg-green-500" : "bg-red-500") : "bg-zinc-950"} h-24 w-24 border-2 border-white rounded-2xl flex items-center justify-center cursor-pointer select-none`}
     >
       <h1 className={`${props.value === 'X' ? 'text-green-500' : 'text-red-500'} font-bold text-7xl`}>{props.value}</h1>
     </div>
